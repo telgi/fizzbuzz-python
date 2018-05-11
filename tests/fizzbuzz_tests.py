bd1d0741d6ba597df89752d6949cf6eff7a150d2
Terry Allitt <tallitt88@gmail.com>
2018-05-11 11:28:43 +0100

    :recycle: Add more assertions to each test case

--- tests/fizzbuzz_tests.py
+++ tests/fizzbuzz_tests.py
@@ -5,18 +5,26 @@ def test_multiples_of_3():
     fizzbuzz = Fizzbuzz()
 
     assert_equal(fizzbuzz.play(3), 'Fizz')
+    assert_equal(fizzbuzz.play(6), 'Fizz')
+    assert_equal(fizzbuzz.play(9), 'Fizz')
 
 def test_multiples_of_5():
     fizzbuzz = Fizzbuzz()
 
     assert_equal(fizzbuzz.play(5), 'Buzz')
+    assert_equal(fizzbuzz.play(10), 'Buzz')
+    assert_equal(fizzbuzz.play(20), 'Buzz')
 
 def test_multiples_of_3_and_5():
     fizzbuzz = Fizzbuzz()
 
     assert_equal(fizzbuzz.play(15), 'FizzBuzz')
+    assert_equal(fizzbuzz.play(30), 'FizzBuzz')
+    assert_equal(fizzbuzz.play(45), 'FizzBuzz')
 
 def test_all_other_numbers():
     fizzbuzz = Fizzbuzz()
 
     assert_equal(fizzbuzz.play(1), 1)
+    assert_equal(fizzbuzz.play(8), 8)
+    assert_equal(fizzbuzz.play(13), 13)
