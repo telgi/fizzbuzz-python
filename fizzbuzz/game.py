6d80d96c2e230bb83383559dc4f93c565eae9179
Terry Allitt <tallitt88@gmail.com>
2018-05-11 10:50:40 +0100

    :white_check_mark: Write and pass first test - return Fizz for multiples of 3

---
+++ fizzbuzz/game.py
@@ -0,0 +1,8 @@
+class Fizzbuzz(object):
+
+    def __init__(self):
+        pass
+
+    def play(self, number):
+        if number % 3 == 0:
+            return 'Fizz'
--- tests/fizzbuzz_tests.py
+++ tests/fizzbuzz_tests.py
@@ -1,2 +1,7 @@
 from nose.tools import *
 from fizzbuzz.game import Fizzbuzz
+
+def test_multiples_of_3():
+    fizzbuzz = Fizzbuzz()
+
+    assert_equal(fizzbuzz.play(3), 'Fizz')
